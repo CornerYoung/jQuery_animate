2eac7975769c9a44b9c75d3239dd0c8b1a63f632
ygn <947498533@qq.com>
2018-07-24 09:40:38 +0800

    根据当前点击元素判断

--- js/jq_animate.js
+++ js/jq_animate.js
@@ -1,12 +1,14 @@
 $(function(){
-   
+   let page = 1;
+   let i = 4;
     $("button.next").click(function(){
-        let $parent = $(this).parent("div.v-show");
-        let $v_show = $parent.find("div.v-content-list");
-        let $v_content = $parent.find("div.v-content");
+        let $v_show = $("div.v-show");
+        let $v_content = $("div.v-content");
         
-        let v_width = $v_show.width()
-        console.log(v_width)
+        let v_width = $v_content.width();
+        let len = $v_show.find("li").length;
+        let page_count = Math.ceil(len/i);
+        console.log(v_width, len)
     });
 
     
